a13ff70a8c86c8ebe991f8588508943c353a8d6d
Dahye Lee <ldhye9009@gmail.com>
2014-03-17 18:10:47 +0900

    initial kernel (tizen 2.2.1)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 PREEMPT Mon Mar 17 14:03:51 KST 2014"
+#define LINUX_COMPILE_BY "dahye"
+#define LINUX_COMPILE_HOST "dahye-desktop"
+#define LINUX_COMPILER "gcc version 4.8.1 (Ubuntu/Linaro 4.8.1-10ubuntu9) "
